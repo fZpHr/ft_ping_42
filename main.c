c1416cb32523a315ccce6ae68bdc4d32c794dccc
fZpHr <afterrush@live.fr>
2024-08-14 17:22:43 +0200

    md

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-        
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 const char *flags[] = {"--help", "-?", "-v"};
 const char *flags_help[] = {"--help", "-?"};
 
 void flag_help()
 {
-        printf("Usage: ping [OPTION...] HOST ...\n"
+    printf("Usage: ping [OPTION...] HOST ...\n"
            "Send ICMP ECHO_REQUEST packets to network hosts.\n"
            "\n"
            " Options valid for all request types:\n"
@@ -24,14 +27,15 @@ void flag_help()
            "Report bugs to <bug-inetutils@gnu.org>.\n");
 }
 
-int main(int ac, char **av)
+int my_ping(int ac, char **av)
 {
+    int uniq = 0;
     if (ac > 1)
     {
         int flag_found = 0;
         for (int i = 1; i < ac; i++)
         {
-            for (long unsigned int j = 0; j+1 < sizeof(flags) / sizeof(flags[0]); j++)
+            for (long unsigned int j = 0; j + 1 < sizeof(flags) / sizeof(flags[0]); j++)
             {
                 if (!strncmp(av[i], flags_help[j], strlen(flags_help[j])))
                 {
@@ -40,7 +44,13 @@ int main(int ac, char **av)
                 }
                 else if (!strncmp(av[i], "-", 1))
                 {
-                    if (strstr(av[i], flags_help[j]))
+                    if (!strcmp(av[i], "--") && i == 1)
+                    {
+                        flag_found++;
+                        uniq++;
+                        break;
+                    }
+                    if (strstr(av[i], flags_help[j]) || !strncmp(av[i], "---", 3))
                     {
                         printf("ping: unrecognized option '%s'\n", av[i]);
                         flag_found++;
@@ -56,8 +66,53 @@ int main(int ac, char **av)
             printf("ping: unknown host\n");
             return 1;
         }
-
     }
+    if (ac == 1 || uniq)
+        printf("ping: missing host operand\n");
     printf("Try 'ping --help' or 'ping --usage' for more information.\n");
     return 64;
+
+}
+
+int ext_ping(char **av)
+{
+    char *ping = "/usr/local/bin/ping"; // Path to the ping executable
+    av[0] = "ping"; // Set the first argument to "ping"
+
+    // Execute ping with the provided arguments
+    int ret = execve(ping, av, NULL);
+    if (ret == -1)
+    {
+        perror("execve");
+        return 1;
+    }
+    return 0;
+}
+
+int main(int ac, char **av)
+{
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        ext_ping(av);
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+        {
+            int ret = WEXITSTATUS(status);
+            printf("\033[0;32m\nRetour: %d\n\033[0m", ret);
+        }
+        printf("-----------------------------\n");
+        printf("\033[0;32m\nRetour: %d\n\033[0m", my_ping(ac, av)); 
+    }
+    return 0;
 }
